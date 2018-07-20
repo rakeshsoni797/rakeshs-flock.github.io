23377f51c5bb78e3d465d9ea14baefc375e53661
Rakesh Soni <rakeshs@flock.com>
2018-07-20 17:06:57 +0530

    Update precache-manifest.900edb774fe482ac0d0d542a95579222.js

--- dist/precache-manifest.900edb774fe482ac0d0d542a95579222.js
+++ dist/precache-manifest.900edb774fe482ac0d0d542a95579222.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "/dist/robots.txt"
   },
   {
     "revision": "b91b581161cba3a228e7",
-    "url": "/js/chunk-vendors.b91b5811.js"
+    "url": "/dist/js/chunk-vendors.b91b5811.js"
   },
   {
     "revision": "4885b006b2de043a286d",
-    "url": "/js/app.4885b006.js"
+    "url": "/dist/js/app.4885b006.js"
   },
   {
     "revision": "141c64c2ee6d9e5508b2570f2aaacfef",
-    "url": "/index.html"
+    "url": "/dist/index.html"
   },
   {
     "revision": "630156b902c23f3bf25adb33469edc98",
-    "url": "/img/profile.630156b9.jpeg"
+    "url": "/dist/img/profile.630156b9.jpeg"
   },
   {
     "revision": "b91b581161cba3a228e7",
-    "url": "/css/chunk-vendors.6c04e2b1.css"
+    "url": "/dist/css/chunk-vendors.6c04e2b1.css"
   },
   {
     "revision": "4885b006b2de043a286d",
-    "url": "/css/app.36f21dd4.css"
+    "url": "/dist/css/app.36f21dd4.css"
   }
 ];
